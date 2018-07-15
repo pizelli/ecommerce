11663f14bee755286602b815cb5fd07ddc0ebb13
Giovanni Pizelli <giovannipizelli@gmail.com>
2018-07-15 14:47:44 -0300

    Tela de login.

--- index.php
+++ index.php
@@ -1,31 +1,46 @@
-<?php 
-
+<?php
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-	
-	$page = new Page();
-
-	$page->setTpl("index");
-	
+    $page = new Page();
+    $page->setTpl("index");
 });
 
 $app->get('/admin', function() {
-	
-	$page = new PageAdmin();
+    User::verifyLogin();
+    $page = new PageAdmin();
+    $page->setTpl("index");
+});
 
-	$page->setTpl("index");
-	
+$app->get('/admin/login', function() {
+    $page = new PageAdmin([
+        'header' => false,
+        'footer' => false
+    ]);
+    $page->setTpl("login");
+});
+
+$app->post('/admin/login', function() {
+    User::login($_POST['login'], $_POST['password']);
+    header("Location: /admin");
+    exit;
+});
+
+$app->get('/admin/logout', function(){
+    User::logout();
+    header("Location: /admin/login");
+    exit;
 });
 
 $app->run();
-
- ?>
+?>
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page
     private $tpl;
     private $options = [];
     private $defaults = [
+        'header'=>true,
+        'footer'=>true,
         'data'=>[]
     ];
 
@@ -16,8 +18,8 @@ class Page
     {
         $this->options = array_merge($this->defaults, $opts);
         $config = array(
-            'tpl_dir'   => $_SERVER['DOCUMENT_ROOT'] . '/lojacurso' . $tpl_dir,
-            'cache_dir' => $_SERVER['DOCUMENT_ROOT'] . '/lojacurso/views-cache/',
+            'tpl_dir'   => $_SERVER['DOCUMENT_ROOT'] . $tpl_dir,
+            'cache_dir' => $_SERVER['DOCUMENT_ROOT'] . '/views-cache/',
             'debug'     => true
         );
         Tpl::configure($config);
@@ -26,7 +28,7 @@ class Page
 
         $this->setData($this->options['data']);
 
-        $this->tpl->draw("header");
+        if ($this->options['header'] === true) $this->tpl->draw("header");
     }
 
     public function setTpl($name, $data = array(), $returnHTML = false)
@@ -38,7 +40,7 @@ class Page
 
     public function __destruct()
     {
-        $this->tpl->draw("footer");
+        if ($this->options['footer'] === true) $this->tpl->draw("footer");
     }
 
     private function setData($data = array())
