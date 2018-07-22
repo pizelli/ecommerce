b5bbbb6c61794cccb240fa7f01a989797fbd3100
Giovanni Pizelli <giovannipizelli@gmail.com>
2018-07-22 19:44:30 -0300

    Site Produtos

--- config.php
+++ config.php
@@ -4,5 +4,9 @@ define("DS", DIRECTORY_SEPARATOR);
 define("PATH_ROOT", __DIR__);
 define("PATH_IMGS", $_SERVER["DOCUMENT_ROOT"] . DS . "res" . DS . "site" . DS . "img" . DS . "products" . DS);
 
+function formatPrice(float $vlprice)
+{
+    return number_format($vlprice, 2, ",", ".");
+}
 
 
