fcc8d966530cd5652803d5bf70bc102c0affb507
Giovanni Pizelli <giovannipizelli@gmail.com>
2018-07-23 00:07:33 -0300

    Paginacao

--- site.php
+++ site.php
@@ -14,12 +14,23 @@ $app->get('/', function() {
 
 
 $app->get('/categories/:idcategory', function($idcategory){
+    $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
     $cat = new Category;
     $cat->get((int)$idcategory);
+    $pagination = $cat->getProductsPage($page);
+    $pages = [];
+    for ($i = 1; $i <= $pagination['pages']; $i++)
+    {
+        array_push($pages, [
+            'link'=>"/categories/{$idcategory}?page={$i}",
+            'page'=>$i
+        ]);
+    }
     $page = new Page;
     $page->setTpl("category", array(
         'category' => $cat->getValues(),
-        'products'=> Product::checkList($cat->getProducts())
+        'products' => $pagination['data'],
+        'pages' => $pages
     ));
 });
 
