9dfa5862fa836483919e24fcd15e230823a59c9f
Giovanni Pizelli <giovannipizelli@gmail.com>
2018-07-27 00:02:15 -0300

    Carrinho Produtos

--- site.php
+++ site.php
@@ -46,9 +46,43 @@ $app->get("/products/:desurl", function($desurl){
 });
 
 $app->get("/cart", function(){
-    Cart::getFromSession();
+    $cart = Cart::getFromSession();
     $page = new Page();
-    $page->setTpl("cart");
+    $page->setTpl("cart", [
+        'cart'=>$cart->getValues(),
+        'products'=>$cart->getProducts()
+    ]);
+});
+
+$app->get("/cart/:idproduct/add", function($idproduct){
+    $product = new Product;
+    $product->get((int)$idproduct);
+    $cart = Cart::getFromSession();
+    $qtd = (isset($_GET['quantity'])) ? (int)$_GET['quantity'] : 1;
+    for($i=0;$i<$qtd;$i++)
+    {
+        $cart->addProduct($product);
+    }
+    header("Location: /cart");
+    exit;
+});
+
+$app->get("/cart/:idproduct/minus", function($idproduct){
+    $product = new Product;
+    $product->get((int)$idproduct);
+    $cart = Cart::getFromSession();
+    $cart->removeProduct($product);
+    header("Location: /cart");
+    exit;
+});
+
+$app->get("/cart/:idproduct/remove", function($idproduct){
+    $product = new Product;
+    $product->get((int)$idproduct);
+    $cart = Cart::getFromSession();
+    $cart->removeProduct($product, true);
+    header("Location: /cart");
+    exit;
 });
 
 ?>
