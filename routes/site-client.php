272e141ed728a9124f5b794276a05c736d32763a
Giovanni Pizelli <giovannipizelli@gmail.com>
2018-07-29 23:33:10 -0300

    Alterar a senha

--- routes/site-client.php
+++ routes/site-client.php
@@ -158,3 +158,46 @@ $app->post("/profile", function(){
     header("Location: /profile");
     exit;
 });
+
+$app->get("/profile/change-password", function(){
+    User::verifyLogin(false);
+    $page = new Page;
+    $page->setTpl("profile-change-password",[
+        'changePassError' => User::getError(),
+        'changePassSuccess' => User::getSuccess()
+    ]);
+});
+
+$app->post("/profile/change-password", function(){
+    User::verifyLogin(false);
+    if(!isset($_POST['current_pass']) || $_POST['current_pass'] === ''){
+        User::setError("Digite a senha atual.");
+        goURL('/profile/change-password');
+    }
+    if(!isset($_POST['new_pass']) || $_POST['new_pass'] === ''){
+        User::setError("Digite a nova senha.");
+        goURL('/profile/change-password');
+    }
+    if(!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === ''){
+        User::setError("Confirme a nova senha.");
+        goURL('/profile/change-password');
+    }
+    if($_POST['new_pass'] !== $_POST['new_pass_confirm']){
+        User::setError("As senhas não estão iguais.");
+        goURL('/profile/change-password');
+    }
+    if($_POST['current_pass'] === $_POST['new_pass']){
+        User::setError("A sua nova senha deve ser diferente da atual.");
+        goURL('/profile/change-password');
+    }
+    $user = User::getFromSession();
+
+    if(!password_verify($_POST['current_pass'], $user->getdespassword())){
+        User::setError("A senha está inválida.");
+        goURL('/profile/change-password');
+    }
+    $user->setdespassword(User::getPasswordHash($_POST['new_pass']));
+    $user->update();
+    User::setSuccess("Senha alterada com sucesso.");
+    goURL("/profile/change-password");
+});
